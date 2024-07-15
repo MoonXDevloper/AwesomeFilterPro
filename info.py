f961ec8c78cea7c81cbc41d621130edd738a2e0d
MoonXDevloper <ak3515400@gmail.com>
2024-07-15 07:50:48 -0700

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '8248586'))
 API_HASH = environ.get('API_HASH', '009edb726ee4baee08d771c35cbd31d3')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6377286706:AAFCVRMQhlJVWSzuuloHKpIPoPTxcdzMciw')
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
